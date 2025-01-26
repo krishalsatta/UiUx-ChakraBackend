790e967059d38e243f4dede13f0546437feef2fe
krishalsatta <sattakrishal97@gmail.com>
2025-01-26 12:48:45 +0545

    commit

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,138 +1,130 @@
-
-const userModel = require('../models/userModel');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const sendOtp = require('../service/sendOtp');
+const userModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const sendOtp = require("../service/sendOtp");
 
 const createUser = async (req, res) => {
-    // res.send("Create user API is working!")
+  // res.send("Create user API is working!")
 
-    //1.Check incoming data
-    console.log(req.body);
+  //1.Check incoming data
+  console.log(req.body);
 
-    //2.Destructure the incoming data
-    const { firstName, lastName, email, phone, password } = req.body;
+  //2.Destructure the incoming data
+  const { firstName, lastName, email, phone, password } = req.body;
 
-    //3.Validate the data
-    if (!firstName || !lastName || !email || !password || !phone) {
-        // res.send("Please enter all fields!")
-        res.status(400).json({
-            "success": false,
-            "message": "Please enter all fields!"
-        });
-        return;
+  //3.Validate the data
+  if (!firstName || !lastName || !email || !password || !phone) {
+    // res.send("Please enter all fields!")
+    res.status(400).json({
+      success: false,
+      message: "Please enter all fields!",
+    });
+    return;
+  }
+
+  //4.Error Handling(try catch)
+  try {
+    //5.Check if the user is already registered
+    const existingUser = await userModel.findOne({ email: email });
+    //5.1 If user found: send response
+    if (existingUser) {
+      return res.json({
+        success: false,
+        message: "User already exists!",
+      });
     }
 
-    //4.Error Handling(try catch)
-    try {
-        //5.Check if the user is already registered
-        const existingUser = await userModel.findOne({ email: email });
-        //5.1 If user found: send response
-        if (existingUser) {
-            return res.json({
-                "success": false,
-                "message": "User already exists!"
-            });
-        }
+    // Hashing / encryption of the password
+    const randomSalt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, randomSalt);
 
-        // Hashing / encryption of the password
-        const randomSalt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, randomSalt);
+    //5.2 If user is new:
 
-        //5.2 If user is new:
+    const newUser = new userModel({
+      //Field : Client's Value
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phone: phone,
+      password: hashedPassword,
+    });
 
-        const newUser = new userModel({
-            //Field : Client's Value
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            phone: phone,
-            password: hashedPassword
-        });
+    //Save to database
+    await newUser.save();
 
-        //Save to database
-        await newUser.save();
-
-        //send the response
-        res.status(201).json({
-            "success": true,
-            "message": "User Created Successfully!"
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-            "success": false,
-            "message": "Internal server Error!"
-        });
-    }
+    //send the response
+    res.status(201).json({
+      success: true,
+      message: "User Created Successfully!",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      message: "Internal server Error!",
+    });
+  }
 };
 
 //login function
 const loginUser = async (req, res) => {
-    // res.send("Login API is working !")
+  // res.send("Login API is working !")
 
-    //Check incoming data
-    console.log(req.body);
+  //Check incoming data
+  console.log(req.body);
 
-    //Destructuring the data
-    const { email, password } = req.body;
+  //Destructuring the data
+  const { email, password } = req.body;
 
-    //Validation
-    if (!email || !password) {
-        return res.json({
-            "success": false,
-            "message": "Please enter all fields!"
-        });
+  //Validation
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "Please enter all fields!",
+    });
+  }
+
+  //try catch
+  try {
+    //find user (email)
+    const user = await userModel.findOne({ email: email });
+    //found data: firstName, lastName, email, password
+
+    //not found(error message)
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User does not exist!",
+      });
     }
 
-    //try catch
-    try {
+    //compare password(bcrypt)
+    const isValidPassword = await bcrypt.compare(password, user.password);
 
-        //find user (email)
-        const user = await userModel.findOne({ email: email });
-        //found data: firstName, lastName, email, password
-
-        //not found(error message)
-        if (!user) {
-            return res.status(400).json({
-                "success": false,
-                "message": "User does not exist!"
-            });
-        }
-
-        //compare password(bcrypt)
-        const isValidPassword = await bcrypt.compare(password, user.password);
-
-        //not valid(error)
-        if (!isValidPassword) {
-            return res.status(400).json({
-                "success": false,
-                "message": "Password not matched!"
-            });
-        }
-        //token(Generate - user Data+KEY)
-        const token = await jwt.sign(
-            { id: user._id },
-            process.env.JWT_SECRET
-        );
-        //response (token, user data)
-        res.status(201).json({
-            "success": true,
-            "message": "User logged in successfully!",
-            "token": token,
-            "userData": user
-        });
-
-
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json({
-            "success": false,
-            "message": "Please enter all fields!"
-        });
+    //not valid(error)
+    if (!isValidPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Password not matched!",
+      });
     }
+    //token(Generate - user Data+KEY)
+    const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    //response (token, user data)
+    res.status(201).json({
+      success: true,
+      message: "User logged in successfully!",
+      token: token,
+      userData: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      success: false,
+      message: "Please enter all fields!",
+    });
+  }
 };
-
 
 const forgotPassword = async (req, res) => {
   const { phone } = req.body;
@@ -225,96 +217,99 @@ const verifyOtpAndSetNewPassword = async (req, res) => {
       message: "Internal server error!",
     });
   }
-};const getUserProfile = async (req, res) => {
-    const token = req.headers.authorization.split(' ')[1]; // Assuming Bearer token
-    if (!token) {
-        return res.status(401).json({ message: 'Authorization token is missing' });
+};
+const getUserProfile = async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1]; // Assuming Bearer token
+  if (!token) {
+    return res.status(401).json({ message: "Authorization token is missing" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await userModel.findById(decoded.id);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        res.json(user);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
-    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 const updateUserProfile = async (req, res) => {
-    const token = req.headers.authorization.split(' ')[1]; // Assuming Bearer token
-    if (!token) {
-        return res.status(401).json({ message: 'Authorization token is missing' });
+  const token = req.headers.authorization.split(" ")[1]; // Assuming Bearer token
+  if (!token) {
+    return res.status(401).json({ message: "Authorization token is missing" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await userModel.findById(decoded.id);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
 
-        const { firstName, lastName, phone, password } = req.body;
-        if (firstName) user.firstName = firstName;
-        if (lastName) user.lastName = lastName;
-        if (phone) user.phone = phone;
-        if (password) user.password = await bcrypt.hash(password, 10);
+    const { firstName, lastName, phone, password } = req.body;
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (phone) user.phone = phone;
+    if (password) user.password = await bcrypt.hash(password, 10);
 
-        await user.save();
-        res.json(user);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
-    }
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 const getUserDetails = async (req, res) => {
-    try {
-      const userId = req.params.id; // Assume you have user ID available in params
-      const user = await User.findById(userId).select('firstName, lastName, phone');
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.json(user);
-    } catch (error) {
-      res.status(500).json({ message: 'Server error', error });
+  try {
+    const userId = req.params.id; // Assume you have user ID available in params
+    const user = await User.findById(userId).select(
+      "firstName, lastName, phone"
+    );
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  };
-  
-  // Get User Token
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+// Get User Token
 const getToken = async (req, res) => {
   try {
     console.log(req.body);
     const { id } = req.body;
- 
+
     const user = await userModel.findById(id);
     if (!user) {
       return res.status(400).json({
         success: false,
-        message: 'User not found',
+        message: "User not found",
       });
     }
- 
+
     const jwtToken = await jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
       process.env.JWT_SECRET,
       (options = {
         expiresIn:
           Date.now() + process.env.JWT_TOKEN_EXPIRE * 24 * 60 * 60 * 1000 ||
-          '1d',
+          "1d",
       })
     );
- 
+
     return res.status(200).json({
       success: true,
-      message: 'Token generated successfully!',
+      message: "Token generated successfully!",
       token: jwtToken,
     });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       success: false,
-      message: 'Internal Server Error',
+      message: "Internal Server Error",
       error: error,
     });
   }
@@ -324,41 +319,39 @@ const getCurrentProfile = async (req, res) => {
   // const id = req.user.id;
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decoded =jwt.verify(token,process.env.JWT_SECRET);
-   
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     const user = await userModel.findById(decoded.id);
     if (!user) {
       return res.status(400).json({
         success: false,
-        message: 'User not found',
+        message: "User not found",
       });
     }
     res.status(200).json({
       success: true,
-      message: 'User fetched successfully',
+      message: "User fetched successfully",
       user: user,
     });
   } catch (error) {
     console.log(error);
     res.status(500).json({
       success: false,
-      message: 'Internal server error',
+      message: "Internal server error",
       error: error,
     });
   }
 };
-  
-  
 
 // Exporting
 module.exports = {
-    createUser,
-    loginUser,
-    forgotPassword,
-    verifyOtpAndSetNewPassword,
-    getUserProfile,
-    updateUserProfile,
-    getUserDetails,
-    getToken,
-    getCurrentProfile,
+  createUser,
+  loginUser,
+  forgotPassword,
+  verifyOtpAndSetNewPassword,
+  getUserProfile,
+  updateUserProfile,
+  getUserDetails,
+  getToken,
+  getCurrentProfile,
 };
